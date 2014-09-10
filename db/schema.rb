50d2982a631972a4029d4b8aa350d82057170b81
sunmeg <atpope5@gmail.com>
2014-09-10 14:23:22 -0400

    Step 5 - drop favorite column

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910180955) do
+ActiveRecord::Schema.define(version: 20140910182130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
-    t.string  "title",                    null: false
-    t.string  "author",                   null: false
-    t.boolean "favorite", default: false
+    t.string  "title",  null: false
+    t.string  "author", null: false
     t.integer "rating"
   end
 
